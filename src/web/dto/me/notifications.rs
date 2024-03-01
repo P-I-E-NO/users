b98d6bf9c23a0fc0947527312908a3b91823ea99
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2024-03-01 11:55:46 +0100

    add: creation date in notifications

--- src/web/dto/me/notifications.rs
+++ src/web/dto/me/notifications.rs
@@ -5,14 +5,15 @@ use utoipa::ToSchema;
 #[derive(Serialize, ToSchema)]
 pub struct NotificationResponse {
     pub success: bool,
-    pub notifications: Vec<Notification>
+    pub notifications: Vec<Notification>,
 }
 
 #[derive(sqlx::FromRow, Serialize, Deserialize, ToSchema, Debug)]
 pub struct Notification {
     id: String,
     to_user: String,
-    data: Json<NotificationData>
+    created_at: String,
+    data: Json<NotificationData>,
 }
 
 #[derive(sqlx::Decode, Serialize, Deserialize, ToSchema, Debug)]
@@ -20,5 +21,5 @@ pub struct NotificationData {
     owner: String,
     car_name: String,
     tank_size: String,
-    consumption: String    
+    consumption: String,
 }
--- src/web/models/users.rs
+++ src/web/models/users.rs
@@ -1,6 +1,6 @@
+use nanoid::nanoid;
 use serde::{Deserialize, Serialize};
 use sqlx::{PgExecutor, Row};
-use nanoid::nanoid;
 use utoipa::ToSchema;
 
 use crate::web::dto::me::notifications::Notification;
@@ -11,108 +11,107 @@ pub struct User {
     pub surname: String,
     pub id: String,
     pub password: String,
-    pub propic_url: Option<String>
+    pub propic_url: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, ToSchema)]
-pub struct UserModel{
+pub struct UserModel {
     pub email: String,
     pub name: String,
     pub surname: String,
     pub id: String,
-    pub propic_url: Option<String> 
+    pub propic_url: Option<String>,
 }
 
 impl User {
-
     pub async fn from_email(
-        e: impl PgExecutor<'_>, 
-        id: &str
+        e: impl PgExecutor<'_>,
+        id: &str,
     ) -> Result<Option<User>, sqlx_core::Error> {
-        let query = sqlx
-            ::query("
+        let query = sqlx::query(
+            "
                 select * from users where email = $1
-            ")
-            .bind(&id)
-            .fetch_optional(e).await?;
+            ",
+        )
+        .bind(&id)
+        .fetch_optional(e)
+        .await?;
 
         if let Some(user) = query {
-            Ok(
-                Some(User {
-                    id: user.get("id"),
-                    name: user.get("name"),
-                    surname: user.get("surname"),
-                    email: user.get("email"),
-                    password: user.get("password"),
-                    propic_url: user.get("propic_url")
-                })
-            )
-        }else{
+            Ok(Some(User {
+                id: user.get("id"),
+                name: user.get("name"),
+                surname: user.get("surname"),
+                email: user.get("email"),
+                password: user.get("password"),
+                propic_url: user.get("propic_url"),
+            }))
+        } else {
             Ok(None)
         }
-
     }
 
     pub async fn get_notifications(
         &self,
         e: impl PgExecutor<'_>,
     ) -> Result<Vec<Notification>, sqlx_core::Error> {
-
         let results: Vec<Notification> = sqlx::query_as(
-            "select * from notifications where to_user = $1"
+            "
+            	select
+             	id, to_user, data, to_char(created_at, 'dd-mm-yyyy HH24:MI:SS (utc)') created_at
+             	from notifications where to_user = $1
+             ",
         )
         .bind(&self.id)
         .fetch_all(e)
         .await?;
-    
-        Ok(results)
 
+        Ok(results)
     }
 
     pub async fn add_fcm_token(
         &self,
-        e: impl PgExecutor<'_>, 
-        token: &str
+        e: impl PgExecutor<'_>,
+        token: &str,
     ) -> Result<(), sqlx_core::Error> {
-        
-        sqlx
-            ::query("
+        sqlx::query(
+            "
                 insert into fcm_tokens values ($1, $2)
-            ")
-            .bind(&token)
-            .bind(&self.id)
-            .execute(e).await?;
-    
-        Ok(())
+            ",
+        )
+        .bind(&token)
+        .bind(&self.id)
+        .execute(e)
+        .await?;
 
+        Ok(())
     }
 
     pub async fn from_id(
-        e: impl PgExecutor<'_>, 
-        id: &str
+        e: impl PgExecutor<'_>,
+        id: &str,
     ) -> Result<Option<User>, sqlx_core::Error> {
-        let query = sqlx
-            ::query("
+        let query = sqlx::query(
+            "
                 select * from users where id = $1
-            ")
-            .bind(&id)
-            .fetch_optional(e).await?;
+            ",
+        )
+        .bind(&id)
+        .fetch_optional(e)
+        .await?;
 
         if let Some(user) = query {
-            Ok(
-                Some(User {
-                    id: user.get("id"),
-                    name: user.get("name"),
-                    surname: user.get("surname"),
-                    email: user.get("email"),
-                    password: user.get("password"),
-                    propic_url: user.get("propic_url")
-                })
-            )
-        }else{
+            Ok(Some(User {
+                id: user.get("id"),
+                name: user.get("name"),
+                surname: user.get("surname"),
+                email: user.get("email"),
+                password: user.get("password"),
+                propic_url: user.get("propic_url"),
+            }))
+        } else {
             Ok(None)
         }
-
     }
 
     pub async fn register(
@@ -121,28 +120,29 @@ impl User {
         name: &str,
         surname: &str,
         password_hash: &str,
-    ) ->  Result<String, sqlx_core::Error> {
-    
+    ) -> Result<String, sqlx_core::Error> {
         let user_id = nanoid!(32);
-        sqlx::query("
-            insert into users (id, name, surname, email, password) values 
+        sqlx::query(
+            "
+            insert into users (id, name, surname, email, password) values
             (
-                $1, 
-                $2, 
-                $3, 
+                $1,
+                $2,
+                $3,
                 $4,
                 $5
             )
-        ")
+        ",
+        )
         .bind(&user_id)
         .bind(name)
         .bind(surname)
         .bind(email)
         .bind(password_hash)
-        .execute(e).await?; // in 0.7, `Transaction` can no longer implement `Executor` directly,
-                                    // so it must be dereferenced to the internal connection type.
-    
+        .execute(e)
+        .await?; // in 0.7, `Transaction` can no longer implement `Executor` directly,
+                 // so it must be dereferenced to the internal connection type.
+
         Ok(user_id.to_string())
-    
     }
 }
